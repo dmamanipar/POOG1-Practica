1302dd9e9922b0c6251da92252fa98c621e405a2
davidmp <mamanipari@gmail.com>
2023-04-23 10:32:35 -0500

    Tarea complatada dmp

---
+++ SysPractica/src/main/java/pe/edu/upeu/app/DisplayGraphics.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.app;
+
+/**
+ *
+ * @author EP-Ing_Sist.-CALIDAD
+ */
+import java.awt.*;  
+import javax.swing.JFrame;  
+  
+public class DisplayGraphics extends Canvas{  
+      
+    public void paint(Graphics g) {  
+        g.drawString("Hello",40,40);  
+        setBackground(Color.WHITE);  
+        g.fillRect(130, 30,100, 80);  
+        g.drawOval(30,130,50, 60);  
+        setForeground(Color.RED);  
+        g.fillOval(130,130,50, 60);  
+        g.drawArc(30, 200, 40,50,90,60);  
+        g.fillArc(30, 130, 40,50,180,40);  
+          
+    }  
+        public static void main(String[] args) {  
+        DisplayGraphics m=new DisplayGraphics();  
+        JFrame f=new JFrame();  
+        f.add(m);  
+        f.setSize(400,400);  
+        //f.setLayout(null);  
+        f.setVisible(true);  
+    }  
+  
+} 
